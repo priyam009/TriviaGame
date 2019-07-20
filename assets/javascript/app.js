b8aeef433059a3b6d3cbceae96a9fe9ac9c07e34
Priyam <priyam009@live.com>
2019-07-20 20:32:31 +1000

    Finishing final js working, adding css and bootstrap to style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,67 @@
-//TODO Create an object "questions" with key as question and values as multiple choice options
+//Create an object "questions" with key as question and values as multiple choice options
 var questions = {
-  "Question 1" : ["Option 1", "Option 2", "Answer1", "Option 3"],
-  "Question 2" : ["Option 1", "Answer2", "Option 2", "Option 3"],
-  "Question 3" : ["Answer3", "Option 1", "Option 2", "Option 3"]
+  "Which animal sleeps for only five minutes a day?" : ["Chameleon", "Koala", "Giraffe", "Beaver"],
+  "One human hair can support how many kilograms?" : ["Three", "Five", "Seven", "Nine"],
+  "Which one of these planets rotates clockwise?" : ["Uranus", "Mercury", "Pluto", "Venus"],
+  "What color was Coca-Cola originally?" : ["Red", "Purple", "Beige", "Green"],
+  "Which country contains the most languages?" : ["Papua New Guinea", "China", "Australia", "Jamaica"]
 };
 
+//Create an object "answers" with key as question and value as correct answer
+var correctAnswers = ["Giraffe","Three","Venus","Green","Papua New Guinea"];
 
-//TODO Create an object "answers" with key as question and value as correct answer
-var correctAnswers = ["Answer1","Answer2","Answer3"];
-
-
-//TODO Variables to store count of correct answered "correctCount" and wrong answered "wrongCount"
+//Variables to store count of correct answered "correctCount" and wrong answered "wrongCount"
 var correntCount = 0;
 var incorrectCount = 0;
 var unanswered = 0;
 
-
 var count;    //Variable to start the Questions counter
 var intervalID;   //Variable to hold setInterval
-var waitResult;
+var waitResult;   //Varibale to hold the last answer for 3 seconds
 var time;   //Varialbe to store time for each Question
-var isRunning= false;
+// var gameAudio = $("#game-audio")[0];
+// gameAudio.play();
 
-
-//TODO Button "Play Game" to start the game
+//Button "Play Game" to start the game
 function playGame() {
   $("#play-game").show();
+  
 
   $("#question-text").hide();
   $("#answer-text").hide();
   $("#time-text").hide();
   $("#next-question").hide();
   $("#final-text").hide();
+  $("#reset").hide();
 
   count = 0;
 
   $("#play-game").on("click", function() {
     $("#play-game").off();
     $("#play-game").hide();
+    $("#game-name").hide();
     askQuestion();
   });
 };
 
+//To "Restart" the game again
+function reset() {
+  $("#reset").off();
+  $("#final-text").hide();
+  $("#reset").hide();
 
-//TODO To show question and options as radio buttons
+  count = 0;
+  correntCount = 0;
+  incorrectCount = 0;
+  unanswered = 0;
+
+  askQuestion();
+}
+
+
+//To show question and options as radio buttons
 function askQuestion() {
+  time = 20;
 
   $("#question-text").show();
   $("#answer-text").show();
@@ -52,7 +69,6 @@ function askQuestion() {
   $("#options").empty();
   $("#next-question").hide();
 
-  time = 20;
 
   $("#question").text(Object.keys(questions)[count]);
 
@@ -64,24 +80,21 @@ function askQuestion() {
 
 };
 
-//TODO Increase the counter for next question and wait for x interval
+//Set 20 second interval to answer the question
 function setTimer() {
-
-  //Start 20 seconds timer for the question
+  $("#time-text").text("Time Remaining: " + time + " Seconds");
   intervalID = setInterval(startTimer,1000);
-  console.log(intervalID);
   checkAnswer();
-
 };
 
-//TODO Show time decrement and update to new question when time is up
+//Show time decrement
 function startTimer() {
   time--;
-  $("#time-text").text(time);
+  $("#time-text").text("Time Remaining: " + time + " Seconds");
   stopTimer();
 };
 
-// TODO Check if time is zero
+// Check if time is zero
 function stopTimer() {
   if(time === 0) {
     clearInterval(intervalID);
@@ -89,17 +102,20 @@ function stopTimer() {
   };
 };
 
+//Get the answer from the checked radio button
 function checkAnswer() {
   $("#submitAnswer").on("click", function() {
     var selected = $("input:checked").val();
     // console.log(selected);
     $("#submitAnswer").off();
     clearInterval(intervalID);
+    
     $("#time-text").hide();
     showAnswer(selected);
   })
 };
 
+//When the answer is not selected
 function missedAnswer() {
   $("#submitAnswer").off();
   if(time === 0) {
@@ -113,37 +129,40 @@ function missedAnswer() {
   decision();
 }
 
+//To show the correct and incorrect answers
 function showAnswer(selected) {
   if(questions[Object.keys(questions)[count]][selected] == correctAnswers[count]) {
     $("#question-text").hide();
     $("#time-text").hide();
 
-    $("#timeout-text").append("<p>Yayy!!</p>");
-    $("#timeout-text").append("<p>" + correctAnswers[count] +"</p>");
+    $("#timeout-text").append("<p>Yayy!! You are right</p>");
+    $("#timeout-text").append("<p>" + correctAnswers[count] +" it is</p>");
     correntCount++;
   }
   else {
     $("#question-text").hide();
     $("#time-text").hide();
 
-    $("#timeout-text").append("<p>Nahh!!</p>");
+    $("#timeout-text").append("<p>Nahh!! You are wrong</p>");
     $("#timeout-text").append("<p>The Correct Answer is " + correctAnswers[count] +"</p>");
     incorrectCount++;
   }
   decision();
 };
 
+//To check if there is a next question or should show final results
 function decision() {
   clearInterval(waitResult);
 
   if(Object.keys(questions).length-1 == count) {
-    waitResult = setTimeout(showResult, 2000);
+    waitResult = setTimeout(showResult, 3000);
   }
   else {
     nextQuestion();
   }
 };
 
+//To increase the count variable and call next question
 function nextQuestion() {
   count++;
 
@@ -155,6 +174,7 @@ function nextQuestion() {
   });
 };
 
+//To show the final results with an option to play the game again
 function showResult() {
   $("#next-question").off();
   $("#timeout-text").empty();
@@ -164,11 +184,15 @@ function showResult() {
   $("#incorrectCount-text").text("Incorrect Answers: "+ incorrectCount);
   $("#unanswered-text").text("Unanswered: "+ unanswered);
 
-  $("#play-game").show();
-  $("#play-game").on();
+  $("#reset").show();
+
+  $("#reset").on("click", function() {
+    reset();
+  });
 
 }
 
-
-playGame();
+$(document).ready(function() {
+  playGame();
+});
 
